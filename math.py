57748535648c6e8c04dcbbdbc68b38365e46ddce
Tapabrata-Pal <98637694+Tapabrata-Pal@users.noreply.github.com>
2022-01-29 18:46:47 +0530

    Initial Commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+# Aadition implementation
+def add(x,y):
+    return x+y
+#Substract Implemetntation
+def subtract(x,y):
+    return x-y              # on master
+# multiply implementation
+def multiply(x,y):
+    return x*y          # on Bug456
+# divide implementation
+def divide(x,y):
+    if y==0:              #master
+        return Error
+    else:
+        return x/y
+#Square implementation    
+def square(x,y):
+   return x*x
